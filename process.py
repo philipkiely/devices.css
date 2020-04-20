bfdbf99d7f02877b5c3c6b9ead21fbc56b600f94
Philip Kiely <philip@kiely.xyz>
2020-04-19 21:09:09 -0500

    Update process.py

--- process.py
+++ process.py
@@ -1,22 +1,37 @@
-def scale(word):
-    if word[-2:] == ';\n':
-        return 'calc(' + word[:-2] + ' / #{$size_divisor});\n'
-    elif word[-3:] == ');\n':
-        return '(' + word[:-3] + ' / #{$size_divisor}));\n'
-    return 'calc(' + word + ' / #{$size_divisor})'
+import os
 
+def scale(token):
+    if token[-2:] == ';\n':
+        return 'calc(' + token[:-2] + ' / var(--size-divisor));\n'
+    elif token[-3:] == ');\n':
+        return '(' + token[:-3] + ' / var(--size-divisor));\n'
+    return 'calc(' + token + ' / var(--size-divisor))'
+
+def build_file(scss):
+    out = ':root {\n\t--size-divisor: 3;\n}\n\n'
+    for line in scss:
+        tokens = line.split(' ')
+        for i in range(len(tokens)):
+            if 'px' in tokens[i]:
+                tokens[i] = scale(tokens[i])
+        out += ' '.join(tokens)
+    out += "@media (min-width: 576px) {\n  \
+            :root {\n\t--size-divisor: 2;\n  \
+            }\n}\n\n@media (min-width: 768px) {\n \
+            :root {\n\t--size-divisor: 1.5;\n  \
+            }\n}\n\n@media (min-width: 992px) { \
+            \n  :root {\n\t--size-divisor: 1;\n  \
+            }\n}\n\n@media (min-width: 1200px) { \
+            \n  :root {\n\t--size-divisor: .67;\n  }\n}"
+    return out
 
 if __name__ == '__main__':
     f = open('devices_old.scss', 'r')
     scss = f.readlines()
     f.close()
-    out = '$size_divisor: 2;\n\n'
-    for line in scss:
-        words = line.split(' ')
-        for i in range(len(words)):
-            if 'px' in words[i]:
-                words[i] = scale(words[i])
-        out += ' '.join(words)
+    out = build_file(scss)
     f = open('devices_new.scss', 'w')
     f.write(out)
     f.close()
+    os.system("sass devices_new.scss devices.css")
+
